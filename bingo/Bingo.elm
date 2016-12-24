92768cc4d0838d94405f01e510fd98a1deec85c9
ItamarGronich <ItamarGronich@Gmail.com>
2016-12-24 15:46:47 +0200

    initial program

--- bingo/Bingo.elm
+++ bingo/Bingo.elm
@@ -1,17 +1,62 @@
 module Bingo exposing (..)
 
-import Html exposing (h1,ul,li,p,text,body)
+import Html exposing (h1, ul, li, p, text, body)
+
 
 -- Model
+
+
+type alias Entry =
+    { phrase : String
+    , value : Int
+    }
+
+
+type alias Model =
+    { entries : List Entry
+    }
+
+
+initialModel : Model
+initialModel =
+    { entries = [ Entry "foo" 100 ]
+    }
+
+
+
+-- Update
+
+
+type Msg
+    = NewGame
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        _ ->
+            ( model, Cmd.none )
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( initialModel, Cmd.none )
 
 
 
 -- View
 
 
-view : Model -> Html Msg
+view : Model -> Html.Html Msg
 view model =
-  body [] [ text toString model]
+    p [] [ text (toString model) ]
 
+
+main : Program Never Model Msg
 main =
-    Html.program ({ init :  model, Platform.Cmd.Cmd msg ) ( , update : msg -> model ->  model, Platform.Cmd.Cmd msg ) , subscriptions : model Platform.Sub.Sub msg , view : model Html.Html msg }
+    Html.program
+        { init = init
+        , update = update
+        , subscriptions = (\_ -> Sub.none)
+        , view = view
+        }
